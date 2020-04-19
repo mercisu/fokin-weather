0172f84c2ae3d6c7492f0464b4ea7357836fd11b
merci.su <merci.suyeon@gmail.com>
2020-04-19 14:37:56 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import Loading from './Loading';
 import * as Location from "expo-location";
+import {Alert} from "react-native-web";
 
+
+//React 컴포넌트 class를 정의하려면 React.Component를 상속받아야함
 export default class extends React.Component {
-  getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+  state = {
+      isLoading:true
   };
 
-  componentDidMount() {
+  getLocation = async () => {
+      try {
+          await Location.requestPermissionsAsync();
+          const {
+              coords: {
+                  latitude, longitude
+              }
+          } = await Location.getCurrentPositionAsync();
+          this.setState({ isLoading:false });
+          //기본적으로 setState 메소드는 해당 컴포넌트의 재렌더링(re-render)을 발생
+      } catch (error) {
+          Alert.alert("Cant't find you.", "So sad");
+      }
+  };
+
+
+  //rendered 컴포넌트를 그림
+ componentDidMount() {
     this.getLocation();
   }
 
+  //creating
+  //React.Component의 하위 class에서 반드시 정의해야 하는 메서드
   render() {
-    return <Loading/>;
+     const { isLoading } = this.state;
+     return isLoading ? <Loading/> : null;
   }
 }
